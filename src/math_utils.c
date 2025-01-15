0a0fbea8c389d346b692466b80d60a2ef30c1658
Aymane2001 <b.aymane2014@gmail.com>
2025-01-15 16:32:47 +0100

    Salah commit 2

--- src/math_utils.c
+++ src/math_utils.c
@@ -5,99 +5,107 @@
 #include "../include/math_utils.h"
 
 // modulo normal
-void rsa_mod(mpz_t r, mpz_t d, mpz_t n){
+void rsa_mod(mpz_t r, mpz_t d, mpz_t n)
+{
 
-	mpz_t temp;
-	mpz_init(temp);
+    mpz_t temp;
+    mpz_init(temp);
 
-	mpz_fdiv_q(temp,d,n);
-	mpz_mul(temp,temp,n);
-	mpz_sub(r,d,temp);
-
+    mpz_fdiv_q(temp, d, n);
+    mpz_mul(temp, temp, n);
+    mpz_sub(r, d, temp);
 }
 
-void rsa_mod_ui(mpz_t r, mpz_t d, unsigned long int n){
+void rsa_mod_ui(mpz_t r, mpz_t d, unsigned long int n)
+{
 
-	mpz_t temp;
-	mpz_init(temp);
+    mpz_t temp;
+    mpz_init(temp);
 
-	mpz_fdiv_q_ui(temp,d,n);
-	mpz_mul_ui(temp,temp,n);
-	mpz_sub(r,d,temp);
-
-} 
+    mpz_fdiv_q_ui(temp, d, n);
+    mpz_mul_ui(temp, temp, n);
+    mpz_sub(r, d, temp);
+}
 
 // rop = base^(exp)
-void rsa_pow_ui(mpz_t rop, mpz_t base, unsigned long int exp){
+void rsa_pow_ui(mpz_t rop, mpz_t base, unsigned long int exp)
+{
 
     mpz_set_ui(rop, 1); // Initialiser rop à 1 (base^0 = 1)
     mpz_t temp_base;
     mpz_init_set(temp_base, base); // Copie de base pour manipulation
 
-    while (exp > 0) {
-        if (exp % 2 == 1) { // Si le bit courant de exp est 1
+    while (exp > 0)
+    {
+        if (exp % 2 == 1)
+        {                                 // Si le bit courant de exp est 1
             mpz_mul(rop, rop, temp_base); // rop *= temp_base
         }
         mpz_mul(temp_base, temp_base, temp_base); // temp_base = temp_base^2
-        exp /= 2; // Décaler exp vers la droite (division par 2)
+        exp /= 2;                                 // Décaler exp vers la droite (division par 2)
     }
 
     mpz_clear(temp_base);
+}
 
-} 
-
-void rsa_ui_pow_ui(mpz_t rop, unsigned long int base, unsigned long int exp) {
+void rsa_ui_pow_ui(mpz_t rop, unsigned long int base, unsigned long int exp)
+{
     mpz_t mpz_base;
-    mpz_init_set_ui(mpz_base, base); 
-    rsa_pow_ui(rop, mpz_base, exp);  
-    mpz_clear(mpz_base);            
+    mpz_init_set_ui(mpz_base, base);
+    rsa_pow_ui(rop, mpz_base, exp);
+    mpz_clear(mpz_base);
 }
 
 // Exponentiation modulaire
-void rsa_powm(mpz_t rop, mpz_t base, mpz_t exp,mpz_t modulo){
+void rsa_powm(mpz_t rop, mpz_t base, mpz_t exp, mpz_t modulo)
+{
 
-	mpz_t a;
-	mpz_init(a);
+    mpz_t a;
+    mpz_init(a);
 
-	int l = (int)mpz_sizeinbase(exp,2); // Calculer le nombre des bits de la base 
-	mpz_set_str(a,"1",10); // Initialiser a à 1
+    int l = (int)mpz_sizeinbase(exp, 2); // Calculer le nombre des bits de la base
+    mpz_set_str(a, "1", 10);             // Initialiser a à 1
 
-	for(int i=l-1;i>=0;i--){
-		mpz_mul(a,a,a);
-		rsa_mod(a,a,modulo);
+    for (int i = l - 1; i >= 0; i--)
+    {
+        mpz_mul(a, a, a);
+        rsa_mod(a, a, modulo);
 
-		if(mpz_tstbit(exp,i)){
-			mpz_mul(a,a,base);
-			rsa_mod(a,a,modulo);
-		}
-	}
-	mpz_set(rop,a);
-	mpz_clear(a);
-
+        if (mpz_tstbit(exp, i))
+        {
+            mpz_mul(a, a, base);
+            rsa_mod(a, a, modulo);
+        }
+    }
+    mpz_set(rop, a);
+    mpz_clear(a);
 }
 
-void rsa_powm_ui(mpz_t rop, mpz_t base, unsigned long int exp,mpz_t modulo){
+void rsa_powm_ui(mpz_t rop, mpz_t base, unsigned long int exp, mpz_t modulo)
+{
 
-	mpz_t a,mpz_exp;
-	mpz_inits(a,mpz_exp,NULL);
+    mpz_t a, mpz_exp;
+    mpz_inits(a, mpz_exp, NULL);
 
-	mpz_set_ui(mpz_exp,exp);
-	rsa_powm(rop,base,mpz_exp,modulo);
-	mpz_clears(a,mpz_exp,NULL);
+    mpz_set_ui(mpz_exp, exp);
+    rsa_powm(rop, base, mpz_exp, modulo);
+    mpz_clears(a, mpz_exp, NULL);
+}
 
-} 
-
-void encrypt(mpz_t c, mpz_t m, mpz_t e, mpz_t n) {
+void encrypt(mpz_t c, mpz_t m, mpz_t e, mpz_t n)
+{
     // Effectuer l'exponentiation modulaire pour chiffrer le message
     rsa_powm(c, m, e, n);
 }
 
-void decrypt_standard(mpz_t m, mpz_t c, mpz_t d, mpz_t n) {
+void decrypt_standard(mpz_t m, mpz_t c, mpz_t d, mpz_t n)
+{
     // Déchiffrement : m = c^d mod n
     rsa_powm(m, c, d, n);
 }
 
-void decrypt_crt(mpz_t m, mpz_t c, mpz_t d, mpz_t p, mpz_t q) {
+void decrypt_crt(mpz_t m, mpz_t c, mpz_t d, mpz_t p, mpz_t q)
+{
     mpz_t dp, dq, qinv, m1, m2, h, temp1, temp2;
     mpz_inits(dp, dq, qinv, m1, m2, h, temp1, temp2, NULL);
 
@@ -129,25 +137,41 @@ void decrypt_crt(mpz_t m, mpz_t c, mpz_t d, mpz_t p, mpz_t q) {
     mpz_clears(dp, dq, qinv, m1, m2, h, temp1, temp2, NULL);
 }
 
-void rsa_sign(mpz_t signature,  mpz_t message, mpz_t d, mpz_t n) {
-// Calcul de la signature : signature = message^d mod n
-rsa_powm(signature, message, d, n);}
-
-int verify(mpz_t signature, mpz_t message, mpz_t e, mpz_t n) {
-    mpz_t recovered_message;
-    mpz_init(recovered_message);
-
-    // Calculer le message récupéré à partir de la signature : recovered_message = signature^e mod n
-    rsa_powm(recovered_message, signature, e, n);
-
-    // Comparer le message récupéré avec le message original
-    int is_valid = (mpz_cmp(recovered_message, message) == 0);
-
-    mpz_clear(recovered_message);
-    return is_valid;
+void rsa_sign(mpz_t signature, mpz_t message, mpz_t d, mpz_t n)
+{
+    // Calcul de la signature : signature = message^d mod n
+    rsa_powm(signature, message, d, n);
 }
 
-int main() {
+int verify(mpz_t signature, mpz_t message, mpz_t e, mpz_t n) {
+  mpz_t recovered_message, difference;
+  mpz_init(recovered_message);
+  mpz_init(difference);
+
+  // Calculer le message récupéré à partir de la signature : recovered_message =
+  // signature^e mod n
+
+
+  rsa_powm(recovered_message, signature, e, n);
+
+  // Calculer la différence entre le message récupéré et le message original
+  mpz_sub(difference, recovered_message, message);
+
+  // Vérifier si la différence est égale à zéro
+  unsigned long int diff_val = mpz_get_ui(difference);
+
+  // Si la différence est égale à zéro, alors les deux entiers sont égaux
+  int is_valid = (diff_val == 0);
+
+  mpz_clear(recovered_message);
+  mpz_clear(difference);
+
+  return is_valid;
+}
+
+
+int main()
+{
     // Test des fonctions de base
     mpz_t a, b, c, d;
     mpz_inits(a, b, c, d, NULL);
@@ -174,7 +198,7 @@ int main() {
     mpz_inits(m, e, n, de, ch, p, q, signature, NULL);
 
     // Données d'exemple
-    mpz_set_str(m, "42", 10);  // Message clair
+    mpz_set_str(m, "42", 10);    // Message clair
     mpz_set_str(e, "65537", 10); // Exposant public
     mpz_set_str(n, "3233", 10);  // Module public
     mpz_set_str(de, "2753", 10); // Exposant privé
@@ -199,9 +223,12 @@ int main() {
 
     // Vérification de la signature
     int is_valid = verify(signature, m, e, n);
-    if (is_valid) {
+    if (is_valid)
+    {
         printf("La signature est valide.\n");
-    } else {
+    }
+    else
+    {
         printf("La signature est invalide.\n");
     }
 
