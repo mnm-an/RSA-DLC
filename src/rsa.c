333bc31a0ac721c6af3d1913c12f118b6f58fd6b
mnm-an <65871533+mnm-an@users.noreply.github.com>
2025-01-16 22:27:20 +0100

    Update rsa.c

--- src/rsa.c
+++ src/rsa.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gmp.h>
+#include <time.h>
+#include "math_utils.h"
+
 void encrypt(mpz_t c, mpz_t m, mpz_t e, mpz_t n)
 {
     // Effectuer l'exponentiation modulaire pour chiffrer le message
     rsa_powm(c, m, e, n);
 }
 
-void decrypt_standard(mpz_t m, mpz_t c, mpz_t d, mpz_t n)
+void decrypt(mpz_t m, mpz_t c, mpz_t d, mpz_t n)
 {
     // Déchiffrement : m = c^d mod n
     rsa_powm(m, c, d, n);
@@ -43,34 +49,34 @@ void decrypt_crt(mpz_t m, mpz_t c, mpz_t d, mpz_t p, mpz_t q)
     mpz_clears(dp, dq, qinv, m1, m2, h, temp1, temp2, NULL);
 }
 
-void rsa_sign(mpz_t signature, mpz_t message, mpz_t d, mpz_t n)
+void sign(mpz_t signature, mpz_t message, mpz_t d, mpz_t n)
 {
     // Calcul de la signature : signature = message^d mod n
     rsa_powm(signature, message, d, n);
 }
 
 int verify(mpz_t signature, mpz_t message, mpz_t e, mpz_t n) {
-  mpz_t recovered_message, difference;
-  mpz_init(recovered_message);
-  mpz_init(difference);
+    mpz_t recovered_message, difference;
+    mpz_init(recovered_message);
+    mpz_init(difference);
 
-  // Calculer le message récupéré à partir de la signature : recovered_message =
-  // signature^e mod n
+    // Calculer le message récupéré à partir de la signature : recovered_message =
+    // signature^e mod n
 
 
-  rsa_powm(recovered_message, signature, e, n);
+    rsa_powm(recovered_message, signature, e, n);
 
-  // Calculer la différence entre le message récupéré et le message original
-  mpz_sub(difference, recovered_message, message);
+    // Calculer la différence entre le message récupéré et le message original
+    mpz_sub(difference, recovered_message, message);
 
-  // Vérifier si la différence est égale à zéro
-  unsigned long int diff_val = mpz_get_ui(difference);
+    // Vérifier si la différence est égale à zéro
+    unsigned long int diff_val = mpz_get_ui(difference);
 
-  // Si la différence est égale à zéro, alors les deux entiers sont égaux
-  int is_valid = (diff_val == 0);
+    // Si la différence est égale à zéro, alors les deux entiers sont égaux
+    int is_valid = (diff_val == 0);
 
-  mpz_clear(recovered_message);
-  mpz_clear(difference);
+    mpz_clear(recovered_message);
+    mpz_clear(difference);
 
-  return is_valid;
+    return is_valid;
 }
