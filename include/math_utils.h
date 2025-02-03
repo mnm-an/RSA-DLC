c4c04c87a87e57786464ed49b98f0006bd005cfb
mnm-an <65871533+mnm-an@users.noreply.github.com>
2025-02-03 21:56:02 +0100

    Update math_utils.h

--- include/math_utils.h
+++ include/math_utils.h
@@ -6,20 +6,11 @@
 // Calcul de modulo normal : r = d % n
 void rsa_mod(mpz_t r, mpz_t d, mpz_t n);
 
-// Calcul de modulo avec un entier non GMP : r = d % n (où n est unsigned long int)
-void rsa_mod_ui(mpz_t r, mpz_t d, unsigned long int n);
-
 // Calcul de puissance : rop = base ^ exp
 void rsa_pow_ui(mpz_t rop, mpz_t base, unsigned long int exp);
 
-// Calcul de puissance avec base et exposant non GMP : rop = base ^ exp
-void rsa_ui_pow_ui(mpz_t rop, unsigned long int base, unsigned long int exp);
-
 // Exponentiation modulaire square and multiply always
 void rsa_powm(mpz_t rop, mpz_t base, mpz_t exp, mpz_t modulo);
-
-// Calcul de puissance modulaire avec exposant non GMP : rop = (base ^ exp) % modulo
-void rsa_powm_ui(mpz_t rop, mpz_t base, unsigned long int exp, mpz_t modulo);
 
 // Calcul la relation de bezout : Donnant a et b il retourne u,v tel que a*u + b*v = pgcd
 void extended_euclide(mpz_t gcd, mpz_t x, mpz_t y, const mpz_t a, const mpz_t b);
