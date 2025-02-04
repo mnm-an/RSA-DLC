71159002d414f0dca8bbd077675cd96286e581d7
mnm-an <65871533+mnm-an@users.noreply.github.com>
2025-02-04 15:42:40 +0100

    Update math_utils.c

--- src/math_utils.c
+++ src/math_utils.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <gmp.h>
 #include <time.h>
-
+#include "prime_utils.h"
 
 // modulo normal
 void rsa_mod(mpz_t r, mpz_t d, mpz_t n){
@@ -42,26 +42,22 @@ void rsa_ui_pow_ui(mpz_t rop, unsigned long int base, unsigned long int exp) {
     mpz_clear(mpz_base);            
 }
 
-// Exponentiation modulaire square and multiply always
+// Exponentiation modulaire
 void rsa_powm(mpz_t rop, mpz_t base, mpz_t exp,mpz_t modulo){
 
 	mpz_t a;
-	mpz_inits(a,b,NULL);
+	mpz_init(a);
 
 	int l = (int)mpz_sizeinbase(exp,2); // Calculer le nombre des bits de la base 
 	mpz_set_str(a,"1",10); // Initialiser a à 1
-	mpz_set_str(b,"1",10); // Initialiser b à 1
 
 	for(int i=l-1;i>=0;i--){
 		mpz_mul(a,a,a);
-		rsa_mod(a,a,modulo); // a^2 mod n
+		rsa_mod(a,a,modulo);
 
 		if(mpz_tstbit(exp,i)){
 			mpz_mul(a,a,base);
-			rsa_mod(a,a,modulo); // a*m mod n
-		}else{
-			mpz_mul(b,a,base);
-			rsa_mod(b,a,modulo); // b*m mod n   square and multiply always
+			rsa_mod(a,a,modulo);
 		}
 	}
 	mpz_set(rop,a);
@@ -80,56 +76,73 @@ void rsa_powm_ui(mpz_t rop, mpz_t base, unsigned long int exp,mpz_t modulo){
 
 } 
 
-void modular_inverse(mpz_t result, mpz_t a, mpz_t phi) {
-    mpz_t r, r_prev, t, t_prev, q, temp, temp2;
-    mpz_inits(r, r_prev, t, t_prev, q, temp, temp2, NULL);
+void extended_euclide(mpz_t gcd, mpz_t u, mpz_t v, mpz_t a, mpz_t b) {
+    mpz_t tmp_a, tmp_b, tmp_u, tmp_v, q, r, tmp_q;
+    mpz_inits(tmp_a, tmp_b, tmp_u, tmp_v, q, r, tmp_q, NULL);
 
-    mpz_set(r, phi);        // r = phi
-    mpz_set(r_prev, a);   // r_prev = a
-    mpz_set_ui(t, 0);     // t = 0
-    mpz_set_ui(t_prev, 1); // t_prev = 1
+    mpz_set(tmp_a, a); // Copie a
+    mpz_set(tmp_b, b); // Copie b
 
-    while (mpz_cmp_ui(r, 0) != 0) {
-        // Calcul de q = r_prev / r
-        mpz_div(q, r_prev, r);
+    mpz_set_ui(u, 1);
+    mpz_set_ui(v, 0);
+    mpz_set_ui(tmp_u, 0);
+    mpz_set_ui(tmp_v, 1);
 
-        // Mise à jour r
-        mpz_mul(temp, q, r);
-        mpz_sub(temp2, r_prev, temp);
-        mpz_set(r_prev, r);
-        mpz_set(r, temp2);
+    while (mpz_cmp_ui(tmp_b, 0) != 0) {
+        mpz_fdiv_q(q, tmp_a, tmp_b); // q = tmp_a // tmp_b
+        mpz_set(r, tmp_b);           // r = tmp_b
+        mpz_mod(tmp_b, tmp_a, tmp_b); // tmp_b = tmp_a % tmp_b
+        mpz_set(tmp_a, r);           // tmp_a = r
 
-        // Mise à jour t
-        mpz_mul(temp, q, t);
-        mpz_sub(temp2, t_prev, temp);
-        mpz_set(t_prev, t);
-        mpz_set(t, temp2);
+        // Update u and tmp_u
+        mpz_set(r, tmp_u);          // r = tmp_u
+        mpz_mul(tmp_q, q, tmp_u);   // tmp_q = q * tmp_u
+        mpz_sub(tmp_u, u, tmp_q);   // tmp_u = u - tmp_q
+
+        mpz_set(u, r);              // u = r  l'inverse de a
+
+        // Update v and tmp_v
+        mpz_set(r, tmp_v);          // r = tmp_v
+        mpz_mul(tmp_q, q, tmp_v);   // tmp_q = q * tmp_v
+        mpz_sub(tmp_v, v, tmp_q);   // tmp_v = v - tmp_q
+
+        mpz_set(v, r);              // v = r  l'inverse de b
     }
 
-    // Si r_prev != 1, l'inverse modulaire n'existe pas
-    if (mpz_cmp_ui(r_prev, 1) != 0) {
-        mpz_set_ui(result, 0); // Pas d'inverse modulaire
-    } else {
-        // Si t_prev est négatif, on l'ajuste
-        if (mpz_sgn(t_prev) < 0) {
-            mpz_add(t_prev, t_prev, phi);
-        }
-        mpz_set(result, t_prev);
-    }
+    mpz_set(gcd, tmp_a); // gcd = tmp_a  (a*u + b*v = gcd)
 
-    mpz_clears(r, r_prev, t, t_prev, q, temp, temp2, NULL);
+    mpz_clears(tmp_a, tmp_b, tmp_u, tmp_v, q, r, tmp_q, NULL);
 }
 
-void calculate_phi(mpz_t phi, mpz_t p, mpz_t q) {
-    mpz_t p_minus_1, q_minus_1;
-    mpz_inits(p_minus_1, q_minus_1, NULL);
+void modular_inverse(mpz_t result,mpz_t a,mpz_t phi){
+	mpz_t inv,k,gcd;
+	mpz_inits(inv,k,gcd,NULL);
 
-    // Calculer p-1 et q-1
-    mpz_sub_ui(p_minus_1, p, 1); // p_minus_1 = p - 1
-    mpz_sub_ui(q_minus_1, q, 1); // q_minus_1 = q - 1
+	// Calculer les termes de bezout
+	extended_euclide(gcd, inv, k, a, phi);
 
-    // Calculer phi = (p-1) * (q-1)
-    mpz_mul(phi, p_minus_1, q_minus_1);
+	if(mpz_cmp_ui(gcd,1)==0){
+		mpz_set(result,inv);
+		mpz_mod(result, inv, phi);
 
-    mpz_clears(p_minus_1, q_minus_1, NULL);
+	}else{
+		// Si le pgcd différent de 1
+		mpz_set_ui(result,0);
+		return;
+	}
+
+	mpz_clears(inv,k,gcd,NULL);
+}
+
+void calculate_phi(mpz_t phi,mpz_t p,mpz_t q){
+
+	mpz_t tmp_p,tmp_q;
+	mpz_inits(tmp_p,tmp_q,NULL);
+
+	mpz_sub_ui(tmp_p,p,1);
+	mpz_sub_ui(tmp_q,q,1);
+
+	mpz_mul(phi,tmp_p,tmp_q);
+
+	mpz_clears(tmp_p,tmp_q,NULL);
 }
